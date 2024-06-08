89a1cda8e7c12f3099bd356ce77110bcdcc10dc6
Jose <joseluis.diaz@geokapti.nl>
2024-06-08 14:28:16 -0500

    create basic api

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+
+from config import settings
+
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
