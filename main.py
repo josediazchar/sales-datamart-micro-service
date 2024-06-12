0d2cefdc7594d13abf3d6190a962d3f7d2edbc05
Jose <joseluis.diaz@geokapti.nl>
2024-06-12 05:54:41 -0500

    add lifespan function to load data before start up api service

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.routing import APIRoute
+from firebase_admin import credentials, initialize_app
 from config import settings
-
 from routes import api_router
+from load_data import load_data
+
+cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+initialize_app(cred)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    """
+    print("Loading data to database")
+    load_data()
+
+    yield
+
+    print("shutting down application")
 
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    lifespan=lifespan
 )
 
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
