c81d1c38370c9a0fdaf9cceb6d2272078f72fde6
Jose <joseluis.diaz@geokapti.nl>
2024-06-12 05:52:50 -0500

    add users router for user endpoints

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 from .sales import sales_router
+from .users import user_router
 
 #from app.api.routes import items, login, users, utils
 
 api_router = APIRouter()
 
 api_router.include_router(sales_router, prefix="/sales", tags=["sales"])
+api_router.include_router(user_router, prefix="/users", tags=["users"])
 
 
 
